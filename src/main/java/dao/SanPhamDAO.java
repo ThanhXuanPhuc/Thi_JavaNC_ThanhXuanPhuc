be0e647b87806968716b3d5595c876fa50fa4260
ADMIN <ADMIN@DESKTOP-DMC6CNQ>
2024-11-18 14:04:47 +0700

    2

---
+++ src/main/java/dao/SanPhamDAO.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import context.DbContext;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import model.SanPham;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class SanPhamDAO {
+    Connection conn;
+    PreparedStatement ps;
+    ResultSet rs;
+
+    public ArrayList<SanPham> getTop6() {
+        ArrayList<SanPham> ds = new ArrayList<>();
+        String sql = "select top 6 * from SanPham order by dongia asc";
+        conn = DbContext.getConnection();
+        try {
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                ds.add(new SanPham(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getString(4),rs.getInt(5), rs.getInt(6)));
+            }
+        } catch (Exception ex) {
+            System.out.println("Loi:" + ex.toString());
+        }
+        return ds;
+    }    
+    
+     public ArrayList<SanPham> laySanPhamTheoLoai(int macd) {
+        ArrayList<SanPham> ds = new ArrayList<>();
+        String sql = "select * from SanPham where MaLoai=?";
+        conn = DbContext.getConnection();
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, macd);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                ds.add(new SanPham(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getString(4),rs.getInt(5), rs.getInt(6)));
+            }
+        } catch (Exception ex) {
+            System.out.println("Loi:" + ex.toString());
+        }
+        return ds;
+    }
+    public static void main(String[] args) {
+        SanPhamDAO sDao = new SanPhamDAO();
+        for(SanPham s:sDao.laySanPhamTheoLoai(2))
+        {
+            System.out.println(s);
+        }
+    }
+}
---
+++ src/main/webapp/shared/category.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : Left
+    Created on : Sep 28, 2023, 12:08:09 PM
+    Author     : KHOACNTT
+--%>
+
+<%@page import="model.TheLoai"%>
+<%@page import="java.util.ArrayList"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<div class="card mb-3">
+    <h3 class="card-header">Category</h3>  
+    <ul class="list-group list-group-flush">
+        <%
+            ArrayList<TheLoai> dsTheLoai = (ArrayList<TheLoai>) request.getAttribute("dsTheLoai");
+            for (TheLoai t : dsTheLoai) {
+        %>
+
+        <li class="list-group-item"> <a href="product?MaLoai=<%=t.getMaloai()%>" class="text-decoration-none"> <i class="bi bi-arrow-right-circle"></i> <%=t.getTenloai()%> </a> </li>
+            <%
+                }
+            %>
+    </ul>   
+</div>
